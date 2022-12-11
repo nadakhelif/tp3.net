078da7f7f650c3a92338ec9fe9be30f62c0fe676
nada <nadakhelif61@gmail.com>
2022-12-11 22:55:41 +0100

    first commit

---
+++ Models/Personal_info.cs
@@ -0,0 +1,51 @@
+﻿using System.Data.SQLite;
+
+namespace newProjectTp3.Models
+{
+    public class Personal_info
+    {
+        public List<Person> GetAllPerson()
+        {
+            List<Person> list = new List<Person>();
+            SQLiteConnection connection = new SQLiteConnection("Data Source=C:\\Users\\Nada\\OneDrive\\Desktop\\.net\\2022 GL3 .NET Framework TP3 - SQLite database.db;");
+            connection.Open();
+            
+            using (connection)
+            {
+                var stm = "SELECT * FROM personal_info";
+                SQLiteCommand cmd = new SQLiteCommand(stm, connection);
+                SQLiteDataReader reader = cmd.ExecuteReader();
+                using (reader)
+                {
+                    while (reader.Read())
+                    {
+                        int id = (int)reader["id"];
+                        string first_name = (string)reader["first_name"];
+                        string last_name = (string)reader["last_name"];
+                        string email = (string)reader["email"];
+                        //   DateTime date_birth = Convert.ToDateTime((string)reader["date_birth"].ToString());
+                        string image = (string)reader["image"];
+                        string country = (string)reader["country"];
+                        
+                        list.Add(new Person(id, first_name, last_name, email, image,country));
+                    }
+                }
+            }
+
+            return list;
+        }
+        public Person GetPerson(int id)
+        {
+            List<Person> list = GetAllPerson();
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (list[i].id == id)
+                {
+                    return list[i];
+                }
+            }
+            return null;
+
+        }
+    }
+}
